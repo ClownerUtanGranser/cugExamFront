c5fd0550fef17e9180db4611fccef99bf6233178
casparsylwan <casparsylwan@gmail.com>
2021-10-23 11:09:19 +0200

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoBoxComponent } from './components/info-box/info-box.component';
+import { StartingComponent } from './course/starting/starting.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { QuizFrameComponent } from './pages/quiz-frame/quiz-frame.component';
+import { LoginComponent } from './pages/user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LandingComponent},
+  {path:'course', component: StartingComponent},
+  {path:'info', component: InfoBoxComponent},
+  {path:'exam', component: StartingComponent},
+  {path:'exam/question', component: QuizFrameComponent},
+  {path:'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
