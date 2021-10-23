f7ac0d40e96cabb6ccc9ac751b8c2271579122ac
casparsylwan <casparsylwan@gmail.com>
2021-10-23 11:10:35 +0200

    Mangeng start up appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from './service/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cugExamFront';
+export class AppComponent implements OnInit{
+
+  constructor(private state:StateService){}
+
+  ngOnInit(): void {
+    this.state.setExamStartBasealuesEng();
+    this.state.setExamQuestionsBasealue();
+  }
+  
 }
