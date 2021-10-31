b7e89015da920a9ceab9e679e62dc3bee1225dd5
casparsylwan <casparsylwan@gmail.com>
2021-10-31 13:50:47 +0100

    section component and  starting component routing

---
+++ src/app/course/section/section.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent implements OnInit {
+
+  url:string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((param)=>{
+      this.url = param.material
+      console.log(typeof param.material)
+    } )
+  }
+
+}
