efea648dea996f3573356db0d26438b19d9e9be9
casparsylwan <casparsylwan@gmail.com>
2021-11-20 23:00:02 +0100

    Added navigation

--- src/app/course/section/section.component.ts
+++ src/app/course/section/section.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-section',
@@ -9,13 +9,47 @@ import { ActivatedRoute } from '@angular/router';
 export class SectionComponent implements OnInit {
 
   url:string;
+  page:string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((param)=>{
       this.url = param.material;
+      this.page = param.page
+      if(parseInt(this.page) < 1 ) this.router.navigate(['/course/material'])
     } )
+
+  }
+
+  nextPage()
+  {
+    console.log("Router: ", this.route.params);
+    if(parseInt(this.page) > 0 )
+    {
+      this.router.navigate(['/course/material/',this.url, parseInt(this.page)  +1])
+    }
+    else
+    {
+      this.router.navigate(['/course/material'])
+    }
+    
+  }
+
+  previousPage()
+  {
+    
+    if(parseInt(this.page) > 1 )
+    {
+      this.router.navigate(['/course/material/',this.url, parseInt(this.page)  -1])
+    }
+    else
+    {
+      console.log("Router1: ", this.route);
+      this.router.navigate(['/course/material'])
+    }
+    
   }
 
 }
