d42529bbf654abffe0f14d1ae72712f66990cbe1
casparsylwan <casparsylwan@gmail.com>
2022-02-13 16:21:51 +0100

    Translation and responsiveness

--- src/app/exam/quiz/quiz.component.ts
+++ src/app/exam/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Question } from 'src/app/model/question';
 
 @Component({
@@ -6,7 +6,7 @@ import { Question } from 'src/app/model/question';
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit, OnChanges {
   @Input() lang:string;
   @Input() question:Question;
   @Output() answerEmit = new EventEmitter();
@@ -14,9 +14,26 @@ export class QuizComponent implements OnInit {
 
   selectedAnswer:number = null;
 
-  constructor() { }
+  buttonText:string = "Submit exam"
 
+  constructor() { }
+  
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.changeLang(this.lang);
+  }
+
+  changeLang(lang:string){
+    if(this.lang == 'AR')
+    {
+      this.buttonText = "تقديم الامتحان"
+    }else if(this.lang == 'ENG'){
+      this.buttonText = "Submit exam"
+    }else{
+      this.buttonText = "Lämna in"
+    }
   }
 
   select(index:number)
